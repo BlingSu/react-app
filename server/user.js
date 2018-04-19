3f90d4ab668e01eadefb80ffd96aa2d9e14dbe6e
angelasubi <21978301@qq.com>
2018-04-19 14:18:38 +0800

    feat: 保存登录信息，cookie

--- server/user.js
+++ server/user.js
@@ -5,6 +5,8 @@ const model = require('./model')
 
 const User = model.getModel('user')
 
+const _filter = {'pwd': 0, '_v': 0}
+
 Router.get('/list', (req, res) => {
   User.find({}, (err, doc) => {
     return res.json(doc)
@@ -12,10 +14,11 @@ Router.get('/list', (req, res) => {
 })
 Router.post('/login', (req, res) => {
   const {user, pwd} = req.body
-  User.findOne({user, pwd: md5Pwd(pwd)}, {'pwd': 0}, (err, doc) => {
+  User.findOne({user, pwd: md5Pwd(pwd)}, _filter, (err, doc) => {
     if (!doc) {
       return res.json({code: 1, msg: '用户名或者密码错误'})
     }
+    res.cookie('userid', doc._id)
     return res.json({code: 0, data: doc})
   })
 })
@@ -25,18 +28,38 @@ Router.post('/register', (req, res) => {
     if (doc) {
       return res.json({code: 1, msg: '用户名重复'})
     }
-    User.create({user, pwd:md5Pwd(pwd), type}, (e, d) => {
+    const userModel = new User({user, pwd:md5Pwd(pwd), type})
+    userModel.save((e, d) => {
       if (e) {
         return res.json({code: 1, msg: '后端出错了'})
       }
-      return res.json({code: 0})
+      const {user, type, _id} = d
+      res.cookie('userid', _id)
+      return res.json({code: 0, data: {user, type, _id}})
     })
+    // User.create(, (e, d) => {
+    //   if (e) {
+    //     return res.json({code: 1, msg: '后端出错了'})
+    //   }
+    //   return res.json({code: 0})
+    // })
   })
 })
 
 Router.get('/info', (req, res) => {
   // 检测cookie
-  return res.json({code:1})
+  const {userid} = req.cookies
+  if (!userid) {
+    return res.json({code:1})
+  }
+  User.findOne({_id: userid}, _filter, (err, data) => {
+    if (err) {
+      return res.json({code:1, msg: '后端出错了'})
+    }
+    if (data) {
+      return res.json({code: 0, data: data})
+    }
+  })
 })
 
 function md5Pwd(pwd) {
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -6,13 +6,13 @@ import {getRedirectPath} from '../util'
 const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 const ERROR_MSG = 'ERROR_MSG'
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+const LOAD_DATA = 'LOAD_DATA'
 
 const initState = {
   redirectTo: '',
   isAuth: false,
   msg: '',
   user: '',
-  pwd: '',
   type: ''
 }
 
@@ -23,6 +23,8 @@ export function user(state=initState, action) {
       return {...state, msg: '', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
     case LOGIN_SUCCESS:
       return {...state, msg: '', redirectTo: getRedirectPath(action.payload), isAuth: true, ...action.payload}
+    case LOAD_DATA:
+      return {...state, ...action.payload}
     case ERROR_MSG:
       return {...state, isAuth: false, msg: action.msg}
     default: 
@@ -38,6 +40,10 @@ function registerSuccess(data) {
 }
 function loginSuccess(data) {
   return {type: LOGIN_SUCCESS, payload: data}
+}
+
+export function loadData(userinfo) {
+  return {type: LOAD_DATA, payload: userinfo}
 }
 
 export function login({user, pwd}) {
